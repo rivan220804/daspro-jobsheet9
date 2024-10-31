67391376c5509322b95ff9fbe5dc6e7297388789
rivan220804 <fadillahrivan25@gmail.com>
2024-10-31 08:29:45 +0700

    ArrayNilai23

--- ArrayBilangan23.java
+++ ArrayBilangan23.java
@@ -13,7 +13,7 @@ public class ArrayBilangan23 {
         bil [2] = 7.5;
         bil [3] = 20000;
 
-       for (int i = 0; i < 4; i++) {
+       for (int i = 0; i < bil.length; i++) {
             System.out.println(bil[i]);
        }
     }
---
+++ ArrayNilai23.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+/**
+ * ArrayNilai23
+ */
+public class ArrayNilai23 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i <10; i++) {
+            System.out.println("Nilai akhir ke-"+i+" adalah " + nilaiAkhir[i]);
+        }
+        
+
+    }
+}
