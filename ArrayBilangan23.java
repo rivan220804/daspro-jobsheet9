01155bda3b762c69fcdeebce13121f5f24932136
rivan220804 <fadillahrivan25@gmail.com>
2024-10-31 08:11:08 +0700

    ArrayBilangan23

--- ArrayBilangan23.java
+++ ArrayBilangan23.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 public class ArrayBilangan23 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         
-        int[] bil = new int [4];
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+        double [] bil = {5.0, 12867, 7.5, 20000};
+       
+        bil [0] = 5.0;
+        bil [1] = 12867;
+        bil [2] = 7.5;
+        bil [3] = 20000;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-        
+       for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+       }
     }
 }
